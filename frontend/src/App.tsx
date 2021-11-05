dfb548f16972934b43618862128e2cc2c6ad6a0b
João Victor Schiavon <schiavonj@yahoo.com.br>
2021-11-05 02:16:00 -0300

    final episodio 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,19 +12,20 @@ function App() {
       <div className="container">
         <h1 className="text-primary py-3">Dashboard de vendas</h1>
         
+        
         <div className="row px-3">
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
             <BarChart/>
           </div>
-        </div>
-        <div className="row px-3">
+                  
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
             <DonutChart/>
           </div>
         </div>
-
+        
+        
         <div className="py-3">
           <h2 className="text-primary">Todas vendas</h2>          
         </div>
